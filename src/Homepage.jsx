99f067c48e428a2a20d3990b356b1c398cabbc83
Stanislav <stas.mihno33@gmail.com>
2026-03-09 19:29:46 +0100

    install and setup react router and tailwind css

---
+++ src/Homepage.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Homepage() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold'>
+        Stjordal kvinne og Familielag
+      </div>
+    </>
+  )
+}
+
+export default Homepage
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Homepage from './Homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
