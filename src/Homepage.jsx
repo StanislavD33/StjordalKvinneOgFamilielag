f5f24d3436636d51e567e4f776278e3feb2d2d26
Stanislav <stas.mihno33@gmail.com>
2026-03-15 21:39:43 +0100

    make homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,13 +1,128 @@
 import React from 'react'
 
+import FamilyIcon from './assets/icons/family_icon.png'
+import SamarbeidIcon from './assets/icons/samarbeid_icon.png'
+import CalendarIcon from './assets/icons/calendar_icon.png'
+import HistoryIcon from './assets/icons/history_icon.png'
+import CommunityIcon from './assets/icons/community_icon.png'
+import RetroCafeIcon from './assets/icons/retrocafe_icon.png'
+import ContactIcon from './assets/icons/contact_icon.png'
+
 function Homepage() {
+  const cards = [
+    { title: "Samarbeid med E-Fiks", img: SamarbeidIcon },
+    { title: "Kalender", img: CalendarIcon },
+    { title: "Historikk", img: HistoryIcon },
+    { title: "Felleskap For Ungdom og Foreldre", img: CommunityIcon },
+    { title: "Retrocafe(hver lørdag)", img: RetroCafeIcon },
+    { title: 'Kontakt oss', img: ContactIcon }
+  ]
+
+  console.log(cards)
 
   return (
-    <>
-      <div className='text-3xl font-bold'>
-        Stjordal kvinne og Familielag
+    <section className="bg-white py-16 px-6">
+      <div className="max-w-7xl mx-auto space-y-16">
+
+        {/* HERO */}
+        <div className="grid md:grid-cols-2 gap-10 items-center text-xl">
+
+          <img
+            src={FamilyIcon}
+            alt="Family"
+            className="w-full max-w-6xl mx-auto"
+          />
+          <div className="max-w-md text-left">
+            <h3 className="inline-block uppercase tracking-widest bg-black p-2">
+              Vårt arbeid
+            </h3>
+            <div className="bg-[#1f3a5f] text-white p-2">
+
+
+              <p className="leading-relaxed uppercase">
+                Vi jobber for å skape et åpent og inkluderende fellesskap i
+                Stjørdal. Vi ønsker å forebygge utenforskap, styrke samholdet i
+                nærmiljøet og vise at laget vårt er for alle – uansett alder og
+                bakgrunn.
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+        {/* TEXT SECTIONS */}
+        <div className="grid md:grid-rows-3 gap-12 text-xl text-black leading-relaxed text-left">
+
+          <div>
+            <h4 className="font-semibold mb-1 text-2xl uppercase">Hvem er vi?</h4>
+            <p>
+              Stjørdal Kvinne- og Familielag er et lokalt lag i Norges
+              Kvinne- og Familieforbund.
+            </p>
+            <p className="mb-4">
+              Vi er en engasjert gruppe kvinner og
+              menn som ønsker å skape et inkluderende og trygt fellesskap i
+              nærmiljøet.
+            </p>
+            <p>
+              I dag samarbeider vi med E-fiks Gaming og Driver Retrocafe - en sosial møteplass for ungdom og voksne. Vi er små i antall, men store i engasjement.
+            </p>
+          </div>
+
+          <div>
+            <h4 className="font-semibold mb-1 text-2xl uppercase">Vårt formål</h4>
+            <p className="mb-4">
+              Vårt formål er å styrke fellesskap, tilhørighet og inkludering i
+              Stjørdal. Vi ønsker å bidra til et nærmiljø der både ungdom og voksne blir sett, hørt og verdsatt.
+            </p>
+            <p>
+              Gjennom samarbeid, frivillighet og aktiviteter arbeider
+              vi for å forebygge utenforskap og skape trygge møteplasser for
+              alle.
+            </p>
+          </div>
+
+          <div>
+            <h4 className="font-semibold mb-1 text-2xl uppercase">Vår visjon</h4>
+            <p>
+              Vår visjon er et lokalsamfunn der ingen faller utenfor.
+            </p>
+            <p className="mb-4">
+              Vi ønsker
+              å være en synlig og positiv aktør som bygger broer mellom
+              generasjoner og skaper rom for både historie og nytenkning.
+            </p>
+            <p>
+              Vi tror at små lag kan gjøre en stor forskjell - når vi står sammen.
+            </p>
+          </div>
+
+        </div>
+
+        {/* CARDS */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
+
+          {cards.map((card, i) => (
+            <div
+              key={i}
+              className="bg-[#1f3a5f] rounded-2xl p-6 text-center text-white shadow-lg hover:scale-105 transition hover:shadow-2xl flex flex-col items-center justify-between aspect-[3/4]"
+            >
+              <img
+                src={card.img}
+                alt=""
+                className="h-105 mx-auto mb-6 object-contain"
+              />
+
+              <button className="bg-red-500 px-5 py-2 rounded-full text-sm font-medium hover:bg-red-600 transition">
+                {card.title}
+              </button>
+            </div>
+          ))}
+
+        </div>
+
       </div>
-    </>
+    </section>
   )
 }
 
